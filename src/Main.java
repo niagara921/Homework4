1b1db711da6fa73441f7d1964f5f54e4de472469
Татьяна <niagara921@mail.ru>
2022-08-27 23:13:47 +0500

    Домашнее задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        int i = 1;
-        while (i <= 10) {
-            System.out.print(i++ + " ");
-        }{
+        int i = 0;
+        while (i <= 9) {
+            System.out.print(++i + " ");
+        }
             System.out.println(" ");
-        }
-        int t = 10;
-            while (t >= 1) {
-                System.out.print(t-- + " ");
-            }{
-                System.out.println(" ");
-        }
 
+        for (; i > 0; i--) {
+                System.out.print(i + " ");
+            }
+                System.out.println(" ");
             int g = 2;
             for (; g <=31; g = g + 7){
             System.out.println("Сегодня пятница " + g +" -е число. Нужно подготовить отчет.");
-        }{
-            System.out.println(" ");
         }
+            System.out.println(" ");
+
+          int currentYear = 2022;
+          int oldYear = currentYear -200;
+          int nextYear = currentYear +100;
+          for (int b=79; b <= nextYear; b+=79){
+              if(b >= oldYear){
+                  System.out.println(b);
+              }
+          }
+
+
 
 
     }
