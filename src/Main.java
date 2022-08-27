0723c9d76060e80681dc85bbfb4af80a1ebb24f7
Татьяна <niagara921@mail.ru>
2022-08-27 20:25:39 +0500

    Домашнее задание. Урок 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i++ + " ");
+        }{
+            System.out.println(" ");
+        }
+        int t = 10;
+            while (t >= 1) {
+                System.out.print(t-- + " ");
+            }{
+                System.out.println(" ");
+        }
+
+            int g = 2;
+            for (; g <=31; g = g + 7){
+            System.out.println("Сегодня пятница " + g +" -е число. Нужно подготовить отчет.");
+        }{
+            System.out.println(" ");
+        }
+
+
     }
 }
